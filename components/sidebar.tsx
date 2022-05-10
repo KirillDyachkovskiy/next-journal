86c2a186374e3d7ff30506e28c3c93dcc732fd23
Kirill <kirilldyachkovskiy@gmail.com>
2022-05-10 14:28:21 +0300

    add icont to sidebar component

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { BiListUl, BiTrendingUp } from "react-icons/bi";
+import { RiMessage2Line } from "react-icons/ri";
+import { MdOutlineLocalFireDepartment } from "react-icons/md";
 import s from "../styles/layout/sidebar.module.scss";
 
 const Sidebar: NextPage = () => {
@@ -8,22 +11,34 @@ const Sidebar: NextPage = () => {
       <ul className={s.sidebar__ul}>
         <li className={s.sidebar__li}>
           <Link href="#">
-            <a>Wall</a>
+            <a className={s.sidebar__item}>
+              <MdOutlineLocalFireDepartment className={s.sidebar__icon} />
+              Wall
+            </a>
           </Link>
         </li>
         <li className={s.sidebar__li}>
           <Link href="#">
-            <a>Messages</a>
+            <a className={s.sidebar__item}>
+              <RiMessage2Line className={s.sidebar__icon} />
+              Messages
+            </a>
           </Link>
         </li>
         <li className={s.sidebar__li}>
           <Link href="#">
-            <a>Rating</a>
+            <a className={s.sidebar__item}>
+              <BiTrendingUp className={s.sidebar__icon} />
+              Rating
+            </a>
           </Link>
         </li>
         <li className={s.sidebar__li}>
           <Link href="#">
-            <a>Subscriptions</a>
+            <a className={s.sidebar__item}>
+              <BiListUl className={s.sidebar__icon} />
+              Subscriptions
+            </a>
           </Link>
         </li>
       </ul>
