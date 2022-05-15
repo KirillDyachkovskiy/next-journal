709d90f968c16ac9cdb385f63a1e7b712a8c5860
Kirill <kirilldyachkovskiy@gmail.com>
2022-05-15 21:57:59 +0300

    make project better

--- views/components/ProfileHeader/ProfileHeader.tsx
+++ views/components/ProfileHeader/ProfileHeader.tsx
@@ -1,6 +1,6 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
-import { Button } from "../../ui";
+import { Button, Tab, Tabs } from "../../ui";
 import { CgProfile } from "react-icons/cg";
 import { FiSettings } from "react-icons/fi";
 import { RiMessage2Line } from "react-icons/ri";
@@ -8,23 +8,36 @@ import { RiMessage2Line } from "react-icons/ri";
 import s from "./profileHeader.module.scss";
 
 const ProfileHeader: FC = () => {
+  const [activeTab, setActiveTab] = useState<number>(0);
+
   return (
     <section className={s.profileHeader}>
-      <div className={s.profileHeader__information}>
-        <CgProfile className={s.profileHeader__avatar} />
-        <h1 className={s.profileHeader__name}>Amon Bower</h1>
-        <p className={s.profileHeader__characteristics}>
-          <span className={s.profileHeader__views}>+200</span>
-          <span className={s.profileHeader__subscribers}>2 подпичсика</span>
-        </p>
-        <p className={s.profileHeader__addition}>На проекте с 15 сен. 2016</p>
+      <div className={s.profileHeader__card}>
+        <div className={s.profileHeader__information}>
+          <CgProfile className={s.profileHeader__avatar} />
+          <h1 className={s.profileHeader__name}>Amon Bower</h1>
+          <p className={s.profileHeader__characteristics}>
+            <span className={s.profileHeader__views}>+200</span>
+            <span className={s.profileHeader__subscribers}>2 подпичсика</span>
+          </p>
+          <p className={s.profileHeader__addition}>На проекте с 15 сен. 2016</p>
+        </div>
+        <div className={s.profileHeader__actions}>
+          <Button type="secondary" icon={<FiSettings />}>
+            {" "}
+          </Button>
+          <Button icon={<RiMessage2Line />}>Написать</Button>
+        </div>
       </div>
-      <div className={s.profileHeader__actions}>
-        <Button type="secondary" icon={<FiSettings />}>
-          {" "}
-        </Button>
-        <Button icon={<RiMessage2Line />}>Написать</Button>
-      </div>
+      <Tabs<number>
+        name="ratingDuration"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      >
+        <Tab value={0} label="ПОСТЫ" />
+        <Tab value={1} label="КОММЕНТАРИИ" />
+        <Tab value={2} label="ЕЩЁ ЧТО-ТО" />
+      </Tabs>
     </section>
   );
 };
